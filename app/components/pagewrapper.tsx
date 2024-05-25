c782bbe6d7036aa9c57b2ed1afad99e6918ba6ce
root <root@DESKTOP-I7KCQJ7>
2024-05-25 16:27:22 +0100

    init commit

---
+++ app/components/pagewrapper.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const Pagewrapper = ({children}:{children:ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>  
+  )
+}
+
+export default Pagewrapper
